f58ce13e15af412abceb1eefa55c0d57d922d27d
Kenichi Shintome <tome119+github@gmail.com>
2016-12-06 11:32:51 +0900

    sample08-09を追加

---
+++ chapter06/sample08-09/src/java/beans/Bb.java
@@ -0,0 +1,46 @@
+package beans;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Bb {
+
+    private Integer[] cities;
+    private static Map<String, Integer> items;
+
+    static {
+        items = new LinkedHashMap<>();
+        items.put("札幌", 1);
+        items.put("仙台", 2);
+        items.put("東京", 3);
+        items.put("大阪", 4);
+        items.put("福岡", 5);
+        items.put("沖縄", 6);
+    }
+
+    public Map<String, Integer> getItems() {
+        return items;
+    }
+
+    public String next() {
+        return "output.xhtml";
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(cities);
+    }
+
+    public Integer[] getCities() {
+        return cities;
+    }
+
+    public void setCities(Integer[] cities) {
+        this.cities = cities;
+    }
+}
