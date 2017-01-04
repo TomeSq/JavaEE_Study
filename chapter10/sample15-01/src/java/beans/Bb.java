35a0b64457ada6d37a00905c3239ea6329561f32
Kenichi Shintome <tome119+github@gmail.com>
2017-01-04 16:58:51 +0900

    sample15-01,02を追加

---
+++ chapter10/sample15-01/src/java/beans/Bb.java
@@ -0,0 +1,28 @@
+package beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Bb {
+
+    private String number;
+    private String name;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
