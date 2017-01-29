0a94338aaa0a073b786475dc4ea8787c0ac37af2
TomeSq <tome119+github@gmail.com>
2017-01-29 21:17:10 +0900

    sample17-04を追加

---
+++ chapter12/sample17-04/src/java/beans/LoggerProducer.java
@@ -0,0 +1,21 @@
+package beans;
+
+import java.util.logging.Logger;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+
+@Dependent
+public class LoggerProducer {
+
+    @Inject
+    InjectionPoint point;
+
+    @Produces
+    public Logger getLogger() {
+        String className = point.getMember().getDeclaringClass().getName();
+        Logger logger = Logger.getLogger(className);
+        return logger;
+    }
+}
